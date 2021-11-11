299d71ca8eb68a4dfb5e0a11750fcd912666e18f
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-11-11 12:06:37 +0530

    deleting person using name

---
+++ AddressBook Using SQL/SQLQuery10.sql
@@ -0,0 +1,6 @@
+use address_book_services
+
+delete from Address_Book where first_name='snehal';
+delete from Address_Book where first_name='swapnil';
+delete from Address_Book where first_name='shweta';
+delete from Address_Book where first_name='suresh';
